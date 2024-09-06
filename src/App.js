0b7c67fc3b7c61f0ab38fe48188c71e8d2ed3ead
Vesta-Nassone <vestanassone@gmail.com>
2024-09-06 21:33:35 +0200

    Add the update-profile route.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import VerificationPage from './components/VerificationPage';
 import Dashboard from './components/Dashboard';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Register />} />
           <Route path="/verify" element={<VerificationPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/update-profile" element={<Profile />} />
         </Routes>
       </div>
     </Router>
