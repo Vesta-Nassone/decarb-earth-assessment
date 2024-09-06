c784a267b5f35bda2d92bcf1b6f6fba11d74d1cf
Vesta-Nassone <vestanassone@gmail.com>
2024-09-06 15:57:37 +0200

    Rende the Regiester component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="font-poppins">
+      <Register />
     </div>
   );
 }
