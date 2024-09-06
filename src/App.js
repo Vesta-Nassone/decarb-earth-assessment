6512795139a8ff57b984ee5f75e8f649f8052af9
Vesta-Nassone <vestanassone@gmail.com>
2024-09-06 19:05:58 +0200

    Add the path to verify code.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Register from './components/Register';
+import VerificationPage from './components/VerificationPage'; // Import the VerificationPage component
 
 function App() {
   return (
-    <div className="font-poppins">
-      <Register />
-    </div>
+    <Router>
+      <div className="font-poppins">
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/verify" element={<VerificationPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
