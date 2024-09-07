751976bfa8e8a0105734b4f32f245f245779e501
Vesta-Nassone <vestanassone@gmail.com>
2024-09-07 10:23:01 +0200

    Simulate a loading timer.

--- src/components/SEOChecker.js
+++ src/components/SEOChecker.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
+import SEOCheckerPlaceholder from './loaders/SEOCheckerPlaceholder'; // Make sure this component is imported
 
 const SEOChecker = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate loading delay
+        const timer = setTimeout(() => setLoading(false), 5000);
+        return () => clearTimeout(timer);
+    }, []);
+
     // Get chart options
     const getSEOChartOptions = () => {
         return {
@@ -96,7 +105,24 @@ const SEOChecker = () => {
     };
 
     return (
-        <Chart options={getSEOChartOptions()} series={[{ name: 'SEO Metrics', data: [70, 50, 66, 93, 83, 90] }]} type="bar" height={400} />
+        <div className='bg-white p-6 rounded-lg shadow-custom'>
+            <h2 className='text-xl font-semibold mb-4'>SEO Metrics Overview</h2>
+            <hr className="my-2" />
+            {loading ? (
+                <SEOCheckerPlaceholder />
+            ) : (
+                <div className='flex flex-col lg:flex-row items-center justify-center'>
+                    <div className='lg:w-3/4 w-full mb-4 lg:mb-0'>
+                        <Chart options={getSEOChartOptions()} series={[{ name: 'SEO Metrics', data: [70, 50, 66, 93, 83, 90] }]} type="bar" height={400} />
+                    </div>
+                    <div className='lg:w-1/4 w-full flex justify-center'>
+                        <a href="https://freetools.seobility.net/en/seocheck/decarb.earth">
+                            <img src="https://freetools.seobility.net/widget/widget.png?url=decarb.earth" alt="Seobility Score for decarb.earth" />
+                        </a>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 };
 
