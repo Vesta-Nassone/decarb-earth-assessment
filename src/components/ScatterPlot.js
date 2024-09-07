4451b95182837220c9659cc556f6c4eb876681e2
Vesta-Nassone <vestanassone@gmail.com>
2024-09-07 10:02:30 +0200

    Add a loading simulation for scatter plot.

--- src/components/ScatterPlot.js
+++ src/components/ScatterPlot.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
+import ScatterPlaceholder from './ScatterPlaceholder';
 
 const ScatterPlot = ({ options, series }) => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate loading delay
+        const timer = setTimeout(() => setLoading(false), 2000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className='bg-white p-6 rounded-lg shadow-custom'>
             <h2 className='text-xl font-semibold mb-4'>Atmospheric CO2 (2000 - 2024)</h2>
             <hr className="my-2" />
-            <Chart
-                options={options}
-                series={series}
-                type='scatter'
-                height={350}
-            />
+            {loading ? (
+                <ScatterPlaceholder />
+            ) : (
+                <Chart
+                    options={options}
+                    series={series}
+                    type='scatter'
+                    height={350}
+                />
+            )}
         </div>
     );
 };
